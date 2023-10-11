d4e1dd6c734777f9ad5d71446ac99c858d8f4187
William35er <125413709+William35er@users.noreply.github.com>
2023-10-11 00:26:00 +0000

    finish assignmentdatamodel

--- src/models.py
+++ src/models.py
@@ -1,32 +1,63 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favorites = relationship("Favorites",back_populates ="user" )
+class Characters(Base):
+    __tablename__ = 'characters'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
 
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    vehicle_class = Column(String(250), nullable=False)
+    make = Column(String(250), nullable=False)
+   
+class Favorites(Base):
+    __tablename__ = 'favorites'
+# Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    fav_type = Column(Enum("character","planet"))
+
+    user = relationship("User", back_populates="favorites")
+    planet = relationship("Planet", back_populates="favorited_by")
+    character = relationship("Character", back_populates="favorited_by")
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
